3686a06c48bfd0c287645b90db50ef7fe63e3d6a
tracer-ics2024 <tracer@njuics.org>
2026-06-05 15:58:09 +0800

    >  compile NEMU 123 beginner Linux snow 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:58:09 up 20:51,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -27,7 +27,7 @@ void isa_reg_display() {
     uint32_t i = 0;
     printf("reg-name\t\t hex-value \t\t dec-value\n");
     for(i = 0; i < 32; i++) {
-        printf("%s \t\t 0x%x \t\t %u\n", regs[i], cpu.gpr[i], cpu.gpr[i]);
+        printf("%s \t\t   0x%x    \t\t %u\n", regs[i], cpu.gpr[i], cpu.gpr[i]);
     }
 
     return;
